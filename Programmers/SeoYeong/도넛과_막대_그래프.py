c57025a7559eef1e1d752304b60c72a5b1e5e107
seoyeong98 <youngseogimkk@gmail.com>
2024-02-19 21:46:45 +0900

    도넛과막대그래프 - accuracy 90.4

--- Programmers/SeoYeong/도넛과_막대_그래프.py
+++ Programmers/SeoYeong/도넛과_막대_그래프.py
@@ -18,7 +18,6 @@ https://school.programmers.co.kr/learn/courses/30/lessons/258711
 
 def solution(edges):
 
-    answer = [0, 0, 0, 0]
     answer = {'create_vertex': 0, 'donut': 0, 'bar': 0, 'eight': 0}
 
     def make_graph() -> list:
@@ -63,11 +62,30 @@ def solution(edges):
             answer['eight'] += 1
             return
     
-    for start_node in graph[create_v]:
-        path = find_subgraph([start_node], set())
-        define_graph(path)
+    # for start_node in graph[create_v]:
+    #     path = find_subgraph([start_node], set())
+    #     define_graph(path)
 
-    
+    """ 각 도넛, 막대, 8자 그래프는 고유의 그래프로 존재하고 있고, 각 그래프의 정점끼리 이어진 경우는 없다.
+     따라서 하나의 연결된 덩어리의 path 순서를 갖고있을 필요 없이 각 덩어리의 정점 개수, 간선 개수만 뽑아오고
+     그래프 성질(eg. 막대의 경우 정점, 간선 각각 n, n-1개)에 따라 판별한다 """
+    for start_node in graph[create_v]:
+        print(f"start node with : {start_node}")
+        node, edge = set([]), set([])
+        q = [start_node]
+        while q:
+            sv = q.pop()
+            node.add(sv)
+            for nv in graph[sv]:
+                if (sv, nv) not in edge: 
+                    q.append(nv)
+                    edge.add((sv, nv))
+        print(f"node:{node}, edge:{edge}")
+
+        n_node, n_edge = len(node), len(edge)
+        if n_node == n_edge : answer['donut'] +=1
+        elif n_node - n_edge == 1 : answer['bar'] +=1
+        elif n_node - n_edge == -1 : answer['eight'] +=1
 
     return list(answer.values())
 
@@ -75,9 +93,9 @@ ans = solution([[2, 3], [4, 3], [1, 1], [2, 1]]); print(ans)
 print('\n')
 ans = solution([[4, 11], [1, 12], [8, 3], [12, 7], [4, 2], [7, 11], [4, 8], [9, 6], [10, 11], [6, 10], [3, 5], [11, 1], [5, 3], [11, 9], [3, 8]])
 print(ans)
-
+print('\n')
 ans = solution([[2, 1], [2, 5], [3, 4], [4, 5], [5, 6], [6, 7], [7, 3], [3, 8], [8, 9], [9, 10], [10, 11], [11, 3]])
 print(ans) # [2, 0, 1, 1]
-
+print('\n')
 ans = solution([[2, 1], [1, 3], [2, 4], [4, 5], [2, 6], [6, 7]])
 print(ans) # [2, 0, 3, 0]
