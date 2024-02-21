1f4e99a506169c81bcaac50c290f138cff25b18c
seoyeong98 <youngseogimkk@gmail.com>
2024-02-21 15:27:34 +0900

    refactor: 도넛과 막대 그래프

--- Programmers/SeoYeong/도넛과_막대_그래프.py
+++ Programmers/SeoYeong/도넛과_막대_그래프.py
@@ -38,33 +38,34 @@ def solution(edges):
     create_v = find_create_v()
     answer['create_vertex'] = create_v
     
-    def find_subgraph(path_v: list, path_e: set):
-        sv = path_v[-1]
-        if len(graph[sv]) == 0: return path_v
-        for nv in graph[sv]:
-            if (sv, nv) in path_e: return path_v
-            path_v.append(nv)
-            path_e.add((sv, nv))
-            find_subgraph(path_v, path_e)
-        return path_v
-    
-    def define_graph(path):
-            p_len = len(path)
-            s_len = len(set(path))
+    def first_trial():
+        def find_subgraph(path_v: list, path_e: set):
+            sv = path_v[-1]
+            if len(graph[sv]) == 0: return path_v
+            for nv in graph[sv]:
+                if (sv, nv) in path_e: return path_v
+                path_v.append(nv)
+                path_e.add((sv, nv))
+                find_subgraph(path_v, path_e)
+            return path_v
+        
+        def define_graph(path):
+                p_len = len(path)
+                s_len = len(set(path))
 
-            if p_len == 1 or p_len == s_len:
-                answer['bar'] += 1
+                if p_len == 1 or p_len == s_len:
+                    answer['bar'] += 1
+                    return
+                if (len(path) == 2 and len(set(path)) == 1) or \
+                    p_len - s_len == 1:
+                    answer['donut'] += 1
+                    return
+                answer['eight'] += 1
                 return
-            if (len(path) == 2 and len(set(path)) == 1) or \
-                p_len - s_len == 1:
-                answer['donut'] += 1
-                return
-            answer['eight'] += 1
-            return
-    
-    # for start_node in graph[create_v]:
-    #     path = find_subgraph([start_node], set())
-    #     define_graph(path)
+        
+        for start_node in graph[create_v]:
+            path = find_subgraph([start_node], set())
+            define_graph(path)
 
     """ 각 도넛, 막대, 8자 그래프는 고유의 그래프로 존재하고 있고, 각 그래프의 정점끼리 이어진 경우는 없다.
      따라서 하나의 연결된 덩어리의 path 순서를 갖고있을 필요 없이 각 덩어리의 정점 개수, 간선 개수만 뽑아오고
@@ -72,13 +73,13 @@ def solution(edges):
     for start_node in graph[create_v]:
         print(f"start node with : {start_node}")
         node, edge = set([]), set([])
-        q = [start_node]
-        while q:
-            sv = q.pop()
+        stk = [start_node]
+        while stk:
+            sv = stk.pop()
             node.add(sv)
             for nv in graph[sv]:
                 if (sv, nv) not in edge: 
-                    q.append(nv)
+                    stk.append(nv)
                     edge.add((sv, nv))
         print(f"node:{node}, edge:{edge}")
 
